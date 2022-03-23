9ddb51d9130e952e6fa24acf1c72f49d3baa0749
Danielly Egito <egitodany@gmail.com>
2022-03-23 10:10:50 -0300

    feat: adiciona componente 'MenuButton'

---
+++ src/components/MenuButton/style.js
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+const Container = styled.div `
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 7rem;
+    height: 7rem;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.1);
+    border-radius: 8px;
+    padding: .5rem;
+    transition: all 0.3s ease-out;
+
+    /*No caso, aqui é um gradiente... como que faz?*/
+    background-color: ${(props) => { 
+        switch (props.color) {
+            case 'dog':
+                return '#F9FCF3'
+            
+            case 'cat':
+                return '#FBF9FC'
+
+            default:
+                return '#BED9DD'
+        }
+    }};
+`;
