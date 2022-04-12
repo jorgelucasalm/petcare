1e3469bae3f5058db2f577916b880c850fb47ade
Danielly Egito <egitodany@gmail.com>
2022-04-12 09:20:07 -0300

    refactor: refatora tela de cadastro "Responsável"

--- src/pages/Cadastro/index.js
+++ src/pages/Cadastro/index.js
@@ -1,46 +1,46 @@
-import Logo from "../../assets/img/logo-petcare.png" 
-import Dog from "../../assets/img/welcome-dog.png"
+import Logo from '../../assets/img/logo-petcare.png';
+import Dog from '../../assets/img/welcome-dog.png';
 
-import Input from "../../components/Input";
-import PrimaryButton from "../../components/PrimaryButton";
-import SecundaryButton from "../../components/SecundaryButton";
-import Footer from "../../components/Footer";
-import { Container, Navbar, BlueSection, Buttons, TextBox, WhiteSection } from "./style";
+import Input from '../../components/Input';
+import PrimaryButton from '../../components/PrimaryButton';
+import SecundaryButton from '../../components/SecundaryButton';
+import Footer from '../../components/Footer';
+import { Container, Navbar, BlueSection, Buttons, WhiteSection } from './style';
 
-function Cadastro (){
-    return (
-        <>
-            <Navbar></Navbar>
-            <Container>
-                <WhiteSection>
-                    <header>
-                        <h2>Cadastro</h2>
-                        <p>Responsável</p>
-                    </header>
+function Cadastro() {
+  return (
+    <>
+      <Navbar></Navbar>
+      <Container>
+        <WhiteSection>
+          <header>
+            <h2>Cadastro</h2>
+            <p>Responsável</p>
+          </header>
 
-                    <TextBox>
-                        <Input />
-                        <Input />
-                        <Input />
-                        <Input />
-                        <Input />
-                    </TextBox>
+          <div>
+            <Input placeholder={''} />
+            <Input />
+            <Input />
+            <Input />
+            <Input />
+          </div>
 
-                    <Buttons>
-                        <PrimaryButton>Próximo</PrimaryButton>
-                        <SecundaryButton>Já tenho conta</SecundaryButton>
-                    </Buttons>
-                </WhiteSection>
+          <Buttons>
+            <PrimaryButton>Próximo</PrimaryButton>
+            <SecundaryButton>Já tenho conta</SecundaryButton>
+          </Buttons>
+        </WhiteSection>
 
-                <BlueSection>
-                    <p>Bem vindo ao</p>
-                    <img src={ Logo } alt="logo-petcare"/>
-                    <img src={ Dog } alt="mascote-canino"/>
-                </BlueSection>
-            </Container>
-            <Footer />
-        </>
-    )
+        <BlueSection>
+          <p>Bem vindo ao</p>
+          <img src={Logo} alt="logo-petcare" />
+          <img src={Dog} alt="mascote-canino" />
+        </BlueSection>
+      </Container>
+      <Footer />
+    </>
+  );
 }
 
 export default Cadastro;
