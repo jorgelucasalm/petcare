0d90472b4ebc7af675508355fe272765ddd09b90
Danielly Egito <egitodany@gmail.com>
2022-04-12 10:08:38 -0300

    fix: ajusta responsividade

--- src/pages/NotFound/style.js
+++ src/pages/NotFound/style.js
@@ -1,40 +1,73 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const Container = styled.div`
-    display: flex;
-    flex-direction: row-reverse;
-    flex-wrap: wrap;
-    
-    align-items: center;
-    justify-content: center;
-    overflow-y: hidden;
+  display: flex;
+  flex-direction: row-reverse;
+  flex-wrap: wrap;
+
+  align-items: center;
+  justify-content: center;
+  gap: 3rem;
+  overflow-y: hidden;
+  width: 100%;
+  height: 100vh;
+  padding: 2rem;
+  color: #074b55;
+  background: linear-gradient(180deg, #bdd8dc 0%, #dbf1f4 100%);
+
+  h2 {
+    font-size: 1.1rem;
+    font-weight: 500;
+
+    @media (max-width: 700px) {
+      text-align: center;
+    }
+  }
+
+  p {
     width: 100%;
-    height: 100vh;
-    color: #074B55;
-    background: linear-gradient(180deg, #BDD8DC 0%, #DBF1F4 100%);
+    max-width: 350px;
+    min-width: 300px;
+    font-size: 0.9rem;
+    font-weight: 400;
 
-    h2 {
-        font-size: 1.1rem;
-        font-weight: 500;
+    padding: 0.5rem 0;
+
+    @media (max-width: 700px) {
+      text-align: center;
     }
-
-    p {
-        max-width: 350px;
-        min-width: 320px;
-        font-size: .9rem;
-        font-weight: 400;
-
-        padding: .5rem 0;
-    }
+  }
 `;
 
 export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  justify-content: center;
+
+  img {
+    width: 100%;
+    max-width: 350px;
+    padding: 1rem 0;
+
+    @media (max-width: 700px) {
+      padding: 0 1rem;
+    }
+  }
+
+  div {
     display: flex;
     flex-direction: column;
-    gap: 1rem;
+    justify-content: center;
+  }
+
+  @media (max-width: 700px) {
     align-items: center;
+  }
 `;
 
 export const Dog = styled.img`
-    margin: 4rem;
+  width: 100%;
+  max-width: 16rem;
+  min-width: 12rem;
 `;
