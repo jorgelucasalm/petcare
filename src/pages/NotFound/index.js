be051b87891b0985081f1b7a9fa87279a6b8e503
Danielly Egito <egitodany@gmail.com>
2022-04-12 10:08:38 -0300

    fix: ajusta responsividade

--- src/pages/NotFound/index.js
+++ src/pages/NotFound/index.js
@@ -1,28 +1,29 @@
-import Icon from '../../assets/img/not-found.png'
-import Logo from '../../assets/img/Not-found-logo.png'
+import Icon from '../../assets/img/not-found.png';
+import Logo from '../../assets/img/Not-found-logo.png';
 
-import { Container, Content, Dog } from "./style";
-import PrimaryButton from "../../components/PrimaryButton"
+import { Container, Content, Dog } from './style';
+import PrimaryButton from '../../components/PrimaryButton';
 
-function NotFound () {
-    return(
-        <>
-            <Container>
-                <Content>
-                    <img src={Logo} alt="logo-petcare"/>
-                    <div>
-                        <h2>Há algo errado...</h2>
-                        <p>A página que você procura não está disponível ou foi permanentemente removida.</p>
-                    </div>
-                    <PrimaryButton>Tela Inicial</PrimaryButton>
-                </Content>
+function NotFound() {
+  return (
+    <Container>
+      <Content>
+        <img src={Logo} alt="logo-petcare" />
+        <div>
+          <h2>Há algo errado...</h2>
+          <p>
+            A página que você procura não está disponível ou foi permanentemente
+            removida.
+          </p>
+        </div>
+        <PrimaryButton>Tela Inicial</PrimaryButton>
+      </Content>
 
-                <div>
-                    <Dog src={Icon} alt="erro-404"/>
-                </div>
-            </Container>
-        </>
-    )
+      <div>
+        <Dog src={Icon} alt="erro-404" />
+      </div>
+    </Container>
+  );
 }
 
 export default NotFound;
