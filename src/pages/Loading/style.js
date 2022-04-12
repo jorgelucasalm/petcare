41d0843846a3439f2a2a745ee9ee70bb5fe889ce
Danielly Egito <egitodany@gmail.com>
2022-04-12 09:48:25 -0300

    fix: modifica imagem de carregamento

--- src/pages/Loading/style.js
+++ src/pages/Loading/style.js
@@ -1,42 +1,62 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
-export const Container = styled.div `
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  overflow-y: hidden;
+  height: 100vh;
+  color: #074b55;
+  background: linear-gradient(180deg, #bdd8dc 0%, #dbf1f4 100%);
+
+  h2 {
+    font-size: 1.1rem;
+    font-weight: 600;
+    padding: 0.5rem;
+  }
+
+  p {
+    font-size: 0.9rem;
+    font-weight: 500;
+  }
+
+  div {
     display: flex;
-    flex-direction: column;
     align-items: center;
     justify-content: center;
-    overflow-y: hidden;
-    width: 100%;
-    height: 100vh;
-    color: #074B55;
-    background: linear-gradient(180deg, #BDD8DC 0%, #DBF1F4 100%);
+    padding: auto;
+  }
+`;
 
-    h2 {
-        font-size: 1.1rem;
-        font-weight: 600;
-        padding: .5rem;
+export const Circle = styled.img`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+
+  width: 80%;
+  max-width: 12rem;
+  min-width: 8rem;
+  margin: 4rem;
+  animation: loading 2s linear infinite;
+
+  @keyframes loading {
+    from {
+      transform: rotate(0);
     }
 
-    p {
-        font-size: .9rem;
-        font-weight: 500;
+    to {
+      transform: rotate(360deg);
     }
+  }
+`;
 
-    img {
-        width: 80%;
-        max-width: 12rem;
-        min-width: 8rem;
-        margin: 4rem;
-        animation: loading 5s linear infinite;
-
-        @keyframes loading {
-            from {
-                transform: rotate(0);
-            }
-
-            to {
-                transform: rotate(360deg);
-            }
-        }
-    }
+export const Paw = styled.img`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: fixed;
+  width: 100%;
+  max-width: 8rem;
 `;
