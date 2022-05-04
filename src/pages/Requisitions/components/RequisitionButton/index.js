073629daebdff8d05acd9a687f0f102850168b23
Danielly Egito <egitodany@gmail.com>
2022-05-04 08:03:24 -0300

    refactor: remove importações desnecessárias

--- src/pages/Requisitions/components/RequisitionButton/index.js
+++ src/pages/Requisitions/components/RequisitionButton/index.js
@@ -1,8 +1,4 @@
 import Button from "./style"
-import Clock from "../../assets/img/clock.svg"
-import Check from "../../assets/img/check.svg"
-import Retorno from "../../assets/img/retorno.svg"
-import Cancel from "../../assets/img/cancel.svg"
 
 function SelectButton({children, color, bg}) {
     return (
