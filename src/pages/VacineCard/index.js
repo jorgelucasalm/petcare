3065247364bc88951179543c0f2b3b39f0fd55da
Danielly Egito <egitodany@gmail.com>
2022-05-05 16:08:58 -0300

    fix: remove div vazia

--- src/pages/VacineCard/index.js
+++ src/pages/VacineCard/index.js
@@ -10,86 +10,84 @@ import { Container, Card, Content, Buttons } from './style';
 
 function VacineCard() {
   return (
-    <>
-      <Container>
-        <Card>
-          <header>
-            <h2>Cartão de vacina</h2>
-            <p>lorem ipsum</p>
-          </header>
+    <Container>
+      <Card>
+        <header>
+          <h2>Cartão de vacina</h2>
+          <p>lorem ipsum</p>
+        </header>
 
-          <Content>
+        <Content>
           <VacineButton color="clock" alt="icone-relógio">
-              <span>
-                <img src={Clock} />
-              </span>
+            <span>
+              <img src={Clock} />
+            </span>
 
-              <p>Vacina Lorem</p>
-            </VacineButton>
+            <p>Vacina Lorem</p>
+          </VacineButton>
 
-            <VacineButton color="check" alt="icone-check">
-              <span>
-                <img src={Check} />
-              </span>
+          <VacineButton color="check" alt="icone-check">
+            <span>
+              <img src={Check} />
+            </span>
 
-              <p>Vacina Lorem</p>
-            </VacineButton>
+            <p>Vacina Lorem</p>
+          </VacineButton>
 
-            <VacineButton color="cancel">
-              <span>
-                <img src={Cancel} alt="icone-cancelar"/>
-              </span>
+          <VacineButton color="cancel">
+            <span>
+              <img src={Cancel} alt="icone-cancelar" />
+            </span>
 
-              <p>Vacina Lorem</p>
-            </VacineButton>
+            <p>Vacina Lorem</p>
+          </VacineButton>
 
-            <VacineButton color="return" alt="icone-retorno">
-              <span>
-                <img src={Retorno} />
-              </span>
+          <VacineButton color="return" alt="icone-retorno">
+            <span>
+              <img src={Retorno} />
+            </span>
 
-              <p>Vacina Lorem</p>
-            </VacineButton>
+            <p>Vacina Lorem</p>
+          </VacineButton>
 
-            <VacineButton color="clock" alt="icone-relógio">
-              <span>
-                <img src={Clock} />
-              </span>
+          <VacineButton color="clock" alt="icone-relógio">
+            <span>
+              <img src={Clock} />
+            </span>
 
-              <p>Vacina Lorem</p>
-            </VacineButton>
+            <p>Vacina Lorem</p>
+          </VacineButton>
 
-            <VacineButton color="check" alt="icone-check">
-              <span>
-                <img src={Check} />
-              </span>
+          <VacineButton color="check" alt="icone-check">
+            <span>
+              <img src={Check} />
+            </span>
 
-              <p>Vacina Lorem</p>
-            </VacineButton>
+            <p>Vacina Lorem</p>
+          </VacineButton>
 
-            <VacineButton color="cancel">
-              <span>
-                <img src={Cancel} alt="icone-cancelar"/>
-              </span>
+          <VacineButton color="cancel">
+            <span>
+              <img src={Cancel} alt="icone-cancelar" />
+            </span>
 
-              <p>Vacina Lorem</p>
-            </VacineButton>
+            <p>Vacina Lorem</p>
+          </VacineButton>
 
-            <VacineButton color="return" alt="icone-retorno">
-              <span>
-                <img src={Retorno} />
-              </span>
+          <VacineButton color="return" alt="icone-retorno">
+            <span>
+              <img src={Retorno} />
+            </span>
 
-              <p>Vacina Lorem</p>
-            </VacineButton>
-          </Content>
+            <p>Vacina Lorem</p>
+          </VacineButton>
+        </Content>
 
-          <Buttons>
-            <SecundaryButton>Voltar</SecundaryButton>
-          </Buttons>
-        </Card>
-      </Container>
-    </>
+        <Buttons>
+          <SecundaryButton>Voltar</SecundaryButton>
+        </Buttons>
+      </Card>
+    </Container>
   );
 }
 
