6091751f3b585fce3ae9dfe395faabef39ddbbb6
Danielly Egito <egitodany@gmail.com>
2022-05-05 20:52:07 -0300

    fix: previne conflito na tela de cadastro

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import Login from "./pages/Login";
 import NotFound from "./pages/NotFound";
-import Cadastro from "./pages/Cadastro";
 import Requisitions from "./pages/Requisitions";
 import Agenda from "./pages/Agenda";
 
 function App() {
   return (
     //<Login />
-    //<Cadastro />
     //<Categories />
     //<Requisitions/>
     <Agenda />
