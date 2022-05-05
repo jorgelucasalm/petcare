e0f50a07e9bfa2de7fe2df5b0027ec651331ed3a
Danielly Egito <egitodany@gmail.com>
2022-05-05 20:55:53 -0300

    fix: previne conflito com a tela de cadastro

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import Login from "./pages/Login";
 import NotFound from "./pages/NotFound";
-import Cadastro from "./pages/Cadastro";
 import Requisitions from "./pages/Requisitions";
 import VacineCard from "./pages/VacineCard";
 
