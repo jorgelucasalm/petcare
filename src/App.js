2fa1defd766f740749b700f7aefe520add39599d
Danielly Egito <egitodany@gmail.com>
2022-05-11 07:34:40 -0300

    feat: adiciona tela de agendamento de exames

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { ThemeProvider } from "styled-components";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
+import ExamSchedule from './pages/Categories/components/ExamSchedule'
+import ScheduleAppointment from './pages/Categories/components/AppointmentSchedule'
 
 import dog from "./assets/style/themes/dog";
 import cat from "./assets/style/themes/cat";
@@ -16,7 +18,7 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <Navbar changeTheme={handleTheme} />
-      <Home />
+      <ExamSchedule />
     </ThemeProvider>
   );
 }
