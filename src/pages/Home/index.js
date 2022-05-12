0cdb7d9a0c800abfc377d33a5230fc6f14ac0efc
Jorge Lucas <jorge.lucasalm@gmail.com>
2022-05-11 22:26:35 -0300

    feat: adiciona logica de troca de modais

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -10,22 +10,29 @@ import Footer from './components/Footer'
 import TopSection from "./components/Top-section"
 import { Container, Hub } from './style'
 import { ThemeContext } from 'styled-components'
+import Categories from './components/Categories'
 
 function Home() {
+  const [showModal, setShowModal] = useState(false)
   const { images } = useContext(ThemeContext)
+
+  const openModal = () => {
+    setShowModal(true)
+  }
+
   return (
     <Container>
       <TopSection image={images.pet} />
       <Hub>
-        <Card tittle={"Agendar"} image={Agendar}></Card>
+        <Card tittle={"Agendar"} image={Agendar} onClick={openModal}></Card>
         <Card tittle={"Requisições"} image={Requisicao}></Card>
         <Card tittle={"Cartão de vacina"} image={Vacina}></Card>
         <Card tittle={"Exames"} image={Exame}></Card>
         <Card tittle={"Configuração"} image={Config}></Card>
         <Card tittle={"FAQ"} image={Faq}></Card>
       </Hub>
-
       <Footer></Footer>
+      <Categories showModal={showModal} setShowModal={setShowModal} />
     </Container>
   );
 }
